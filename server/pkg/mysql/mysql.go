229d969d8f5908cad809831c0fed378b006b2d4d
adhxabre <abeldustin06@gmail.com>
2023-04-18 09:34:26 +0700

    Backend

--- server/pkg/mysql/mysql.go
+++ server/pkg/mysql/mysql.go
@@ -2,7 +2,9 @@ package mysql
 
 import (
 	"fmt"
+	"os"
 
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -12,6 +14,15 @@ func DatabaseInit() {
 	var err error
 
 	// if we using mysql driver, reference : https://gorm.io/docs/connecting_to_the_database.html
+
+	var DB_HOST = os.Getenv("DB_HOST")
+	var DB_USER = os.Getenv("DB_USER")
+	var DB_PASSWORD = os.Getenv("DB_PASSWORD")
+	var DB_NAME = os.Getenv("DB_NAME")
+	var DB_PORT = os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	// dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
 	// DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
